832dd1a0e1518bf70807ca368d2972565a802b6f
justinmakaila <justinmakaila@gmail.com>
2014-08-05 16:52:37 -0400

    Added the source

---
+++ PresentColors.swift
@@ -0,0 +1,43 @@
+//
+//  PresentColors.swift
+//  Present
+//
+//  Created by Justin Makaila on 8/5/14.
+//  Copyright (c) 2014 Present. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class func presentDarkGray() -> UIColor {
+        return UIColor(hexString: "444")
+    }
+    
+    class func presentMidGray() -> UIColor {
+        return UIColor(hexString: "777")
+    }
+    
+    class func presentLightGray() -> UIColor {
+        return UIColor(hexString: "AAA")
+    }
+    
+    class func presentOffWhite() -> UIColor {
+        return UIColor(hexString: "F0F0F0")
+    }
+    
+    class func presentCyan() -> UIColor {
+        return UIColor(hexString: "14E6E6")
+    }
+    
+    class func presentBlue() -> UIColor {
+        return UIColor(hexString: "00657A")
+    }
+    
+    class func presentPurple() -> UIColor {
+        return UIColor(hexString: "5207E2")
+    }
+    
+    class func presentPink() -> UIColor {
+        return UIColor(hexString: "F94B7D")
+    }
+}
